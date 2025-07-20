23dbfa0070872c1aa4f36557a6c13dfd4e34227a
Saurabh shinde <117251508+saurabhshinde18@users.noreply.github.com>
2025-07-20 12:00:35 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://https://aicodereview2.onrender.com/ai/get-review', { code })
+    const response = await axios.post('https://aicodereview2.onrender.com/ai/get-review', { code })
     setReview(response.data)
   }
 
